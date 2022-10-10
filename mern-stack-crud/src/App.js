b48afe16b339f60c7bc4b80353ac468f2b5fa065
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-10 14:59:53 -0600

    update App.js to include the menu to make routing in our customer info app

--- mern-stack-crud/src/App.js
+++ mern-stack-crud/src/App.js
@@ -24,25 +24,61 @@ import CustomerForm from "./Components/CustomerForm";
 import CustomerTableRow from "./Components/CustomerTableRow";
 
 // App Component
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/create-customer"}
+                className="nav-link">
+                  Customer Information App
+                </Link>
+              </Navbar.Brand>
+
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-customer"} 
+                    className="nav-link">
+                    Create Customer
+                  </Link>
+                </Nav>
+
+                <Nav>
+                  <Link to={"/customer-list"} 
+                    className="nav-link">
+                    Customer List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route exact path="/"
+                  component={CreateCustomer}/>
+                  <Route path="/CreateCustomer"
+                  component={CreateCustomer}/>
+                  <Route path="/EditCustomer/:id"
+                  component={EditCustomer}/>
+                  <Route path="/CustomerList"
+                  component={CustomerList}/>
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
+    
   );
-}
+};
 
 export default App;
